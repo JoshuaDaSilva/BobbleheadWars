3c63a5c7aee2e941b7914b1a13af7d1ab97da811
Joshua Da Silva <joshua.dasilva@student.sl.on.ca>
2022-09-26 15:45:33 -0400

    Chapter 5 (End) - Tuned Pathfinding

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -8,6 +8,9 @@ public class Alien : MonoBehaviour
     public Transform target;
     private NavMeshAgent agent;
 
+    public float navigationUpdate;
+    private float navigationTime = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +20,15 @@ public class Alien : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (target != null)
+        navigationTime += Time.deltaTime;
+        if (navigationTime > navigationUpdate)
         {
             agent.destination = target.position;
+            navigationTime = 0;
         }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
